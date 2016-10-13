05306765d4b8d373cffe20b6666f2361ebb56e4e
Dalton Dias <dalton.dias@ymail.com>
2016-10-13 15:38:06 -0300

    Tutorial about Toolbar with Material Design

---
+++ app/src/main/java/materialdesign/toolbar/exemplo/br/exempletoolbarmaterialdesign/SecondActivity.java
@@ -0,0 +1,93 @@
+package materialdesign.toolbar.exemplo.br.exempletoolbarmaterialdesign;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    // formato action bar
+    private Toolbar mToolbar;
+
+    // formato standalone
+    private Toolbar mToolbarBottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        mToolbar = (Toolbar) findViewById(R.id.tb_main);
+        mToolbar.setTitle("Second Activity");
+        mToolbar.setSubtitle("Just a subtitle");
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        mToolbarBottom = (Toolbar) findViewById(R.id.inc_tb_bottom);
+        mToolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = null;
+
+                switch (item.getItemId()) {
+                    case R.id.action_facebook:
+                        intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse("http://www.facebook.com"));
+                        break;
+                    case R.id.action_youtube:
+                        intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse("http://www.youtube.com"));
+                        break;
+                    case R.id.action_google_pls:
+                        intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse("http://plus.google.com"));
+                        break;
+                    case R.id.action_linkedin:
+                        intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse("http://www.linkedin.com"));
+                        break;
+                    case R.id.action_whatsapp:
+                        intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse("http://www.whatsapp.com"));
+                        break;
+                }
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        mToolbarBottom.inflateMenu(R.menu.menu_bottom);
+        mToolbarBottom.findViewById(R.id.iv_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SecondActivity.this,"Settings pressed",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_second,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            finish();
+            return true;
+        }
+        return true;
+    }
+}
