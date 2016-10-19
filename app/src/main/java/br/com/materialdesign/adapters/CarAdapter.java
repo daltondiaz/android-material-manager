5d3f5ee335758f1419ce307b0cea79c367a5d871
Dalton Dias <dalton.dias@ymail.com>
2016-10-19 16:58:47 -0200

    Finished CardView Tutorial

--- app/src/main/java/br/com/materialdesign/adapters/CarAdapter.java
+++ app/src/main/java/br/com/materialdesign/adapters/CarAdapter.java
@@ -1,6 +1,9 @@
 package br.com.materialdesign.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +19,7 @@ import java.util.List;
 
 import br.com.materialdesign.R;
 import br.com.materialdesign.domain.Car;
+import br.com.materialdesign.helper.ImageHelper;
 import br.com.materialdesign.intarfaces.RecyclerViewOnClickListenerHack;
 
 /**
@@ -27,13 +31,23 @@ import br.com.materialdesign.intarfaces.RecyclerViewOnClickListenerHack;
 
 public class CarAdapter extends RecyclerView.Adapter<CarAdapter.MyViewHolder> {
 
+    private Context mContext;
     private List<Car> cars ;
     private LayoutInflater layoutInflater;
     private RecyclerViewOnClickListenerHack recyclerViewOnClickListenerHackI;
+    private float scale;
+    private int width;
+    private int height;
 
     public CarAdapter(Context c, List<Car> listCar){
+        mContext = c;
         this.cars = listCar;
         this.layoutInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        scale = mContext.getResources().getDisplayMetrics().density;
+        // Corrigir
+        width = mContext.getResources().getDisplayMetrics().widthPixels  -  (int)(14 + scale + 8.5f);
+        height = (width/16) * 9;
     }
 
     @Override
@@ -52,12 +66,23 @@ public class CarAdapter extends RecyclerView.Adapter<CarAdapter.MyViewHolder> {
         myViewHolder.tvModel.setText(cars.get(position).getModel());
         myViewHolder.tvBrand.setText(cars.get(position).getBrand());
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            myViewHolder.ivCar.setImageResource(cars.get(position).getPhoto());
+        }else{
+            Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(),cars.get(position).getPhoto());
+            bitmap = Bitmap.createScaledBitmap(bitmap,width,height,false);
+
+            //ImageHelper para arredondar a borda
+            bitmap = ImageHelper.getRoundedCornerBitmap(mContext,bitmap,10,width,height,false,false,true,true);
+            myViewHolder.ivCar.setImageBitmap(bitmap);
+        }
+
         try{
             YoYo.with(Techniques.Tada)
                     .duration(700)
                     .playOn(myViewHolder.itemView);
         }catch(Exception e){
-
+            e.printStackTrace();
         }
     }
 
